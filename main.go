b8773a2cb3d55e5caef2e79c77ce7b372edc30a0
Ehioje Henry Erabor <ehiojee@yahoo.com>
2024-10-17 23:59:13 +0100

    lay out the structure of the key value pair as a foundation with interface

--- main.go
+++ main.go
@@ -4,6 +4,19 @@ import (
 	"fmt"
 )
 
+var kv = map[string]interface{}{}
+
+type KeyValueStore interface {
+	Set(key string, value interface{})
+	Get(key string) (interface{}, bool)
+	Del(key string)
+}
+
+type InMemoryStore struct {
+	key   string
+	value interface{}
+}
+
 func main() {
 	fmt.Println("Hello inMemory app")
 }
