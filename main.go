0b801157e2d5561a5497b14d0d48221796cc79a3
Ehioje Henry Erabor <ehiojee@yahoo.com>
2024-10-17 23:59:56 +0100

    implement the interface

--- main.go
+++ main.go
@@ -17,6 +17,27 @@ type InMemoryStore struct {
 	value interface{}
 }
 
+func (i *InMemoryStore) Set(key string, value interface{}) {
+	// implementation
+
+	kv[key] = value
+
+}
+
+func (i *InMemoryStore) Get(key string) (interface{}, bool) {
+	// implementation
+	value, ok := kv[key]
+
+	return value, ok
+
+}
+
+func (i *InMemoryStore) Del(key string) {
+	// implementation
+	delete(kv, key)
+
+}
+
 func main() {
 	fmt.Println("Hello inMemory app")
 }
